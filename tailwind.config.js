18d667cceca2e0b6af14a1eeafbf7af9bc1253f2
alekspanteli <alekspanteli@rambler.ru>
2024-01-11 17:11:56 +0200

    update tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'primary': '#605DC8',
+      'secondary': '#8B89E6',
+      'accent': '#e8e7fa',
+      'shadow-bg': '#E8E8E8',
+    },
   },
   plugins: [],
 }
