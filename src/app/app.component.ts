ea42377c9cdefb5e36c57ec2307daff2a2d8dcdd
alekspanteli <alekspanteli@rambler.ru>
2024-01-11 16:09:24 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,9 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  template: `Hello World`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'current-directory';
+  title = 'default';
 }
