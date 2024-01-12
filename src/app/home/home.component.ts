2406bab59c1c187efeacab041890f85add449c38
alekspanteli <alekspanteli@rambler.ru>
2024-01-12 16:45:15 +0200

    test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,14 +10,14 @@ import { Housinglocation } from '../housinglocation';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  readonly baseUrl = 'https://picsum.photos/';
+  readonly baseUrl = '/assets/tutorials/common';
 
   housingLocation: Housinglocation = {
     id: 9999,
     name: 'Test Home',
     city: 'Test city',
     state: 'ST',
-    photo: `https://picsum.photos/200/300`,
+    photo: `${this.baseUrl}/example-house.jpg`,
     availableUnits: 99,
     wifi: true,
     laundry: false,
